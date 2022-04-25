6e11e8e230682740d3a985c24948a951c04dafac
moraamenge <cynthiamenge96@gmail.com>
2022-04-25 09:53:57 -0700

    singly

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+* free_listint2 - free a listint_t list, sets head to NULL
+* @head: address of head of list
+* Return: nothing
+**/
+void free_listint2(listint_t **head)
+{
+listint_t *tmp;
+
+if (head == NULL)
+return;
+while (*head)
+{
+tmp = (*head)->next;
+free(*head);
+*head = tmp;
+}
+head = NULL;
+}
