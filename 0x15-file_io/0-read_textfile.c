fc84a4ca2b7eb09f0d8fe43d4c05b68d8eaf7549
cindy <cynthiamenge96@gmail.com>
2022-05-03 21:13:33 +0300

    file

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,54 +1,46 @@
 #include "main.h"
 
 /**
- * read_textfile - Function that reads a text file and prints it
- *
- * @filename: The pathname of file to read
- * @letters: number of letters it should read and print
- *
- * Description: A function that reads a text file and prints it to
- * the POSIX standard output.
- * Return: Number of letters it could read and print, otherwise 0
- */
+* read_textfile - reads a text file and prints it to the standard output
+* @filename: name of the file to be read
+* @letters: number of letters to read and print
+*
+* Return: the number of letters printed, or 0 if it failed
+*/
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	/* Declarations */
-	ssize_t file_descriptor, read_bytes, written_bytes;
-	char *buffer;
+int fd;
+int s, t;
+char *buf;
 
-	if (filename == NULL)
-		return (0);
+if (!filename)
+return (0);
 
-	/* Dynamic allocation for string to be written*/
-	buffer = malloc(sizeof(char) * letters);
+fd = open(filename, O_RDONLY);
+if (fd < 0)
+return (0);
 
-	/*Check to avoid avoid segmentation fault*/
-	if (!buffer)
-		return (0);
+buf = malloc(sizeof(char) * letters);
+if (!buf)
+return (0);
 
-	/* Open file in read only using the oflag O_RDONLY */
-	file_descriptor = open(filename, O_RDONLY);
+s = read(fd, buf, letters);
+if (s < 0)
+{
+free(buf);
+return (0);
+}
+buf[s] = '\0';
 
-	/* Read the file*/
-	read_bytes = read(file_descriptor, buffer, letters);
+close(fd);
 
-	/* Output to standard output*/
-	written_bytes = write(STDOUT_FILENO, buffer, letters);
+t = write(STDOUT_FILENO, buf, s);
+if (t < 0)
+{
+free(buf);
+return (0);
+}
 
-	/* Check for fails*/
-	if (read_bytes == -1 || written_bytes == -1 || file_descriptor == -1
-	 || read_bytes != written_bytes)
-	{
-		free(buffer);
-		close(file_descriptor);
-		return (0);
-	}
-
-	/* Close file descriptor */
-	close(file_descriptor);
-
-	/* Free memory to avoid memory leak */
-	free(buffer);
-
-	return (written_bytes);
+free(buf);
+return (t);
 }
